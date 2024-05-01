129f731b0a005c8d85d2478478156ad53f5d975f
samuelazure <durhamstar1@gmail.com>
2024-05-01 20:37:54 +0100

    created nsgvm.tf

---
+++ nsgvm.tf
@@ -0,0 +1,19 @@
+resource "azurerm_network_security_group" "this_rg_nsg" {
+  name                = "var.network_nsg"
+  location            = azurerm_resource_group.this_rg.location
+  resource_group_name = azurerm_resource_group.this_rg.name
+}
+
+resource "azurerm_network_security_rule" "this_rg_nsrule_vm" {
+  name                        = "var.network_nsrule_vm"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.this_rg.name
+  network_security_group_name = azurerm_network_security_group.this_rg_nsg.name
+}
