ed00d79a4749ab4de6f351b0a67de9a6b6e049e0
Eka Amasi <ea22aai@herts.ac.uk>
2024-05-02 22:35:00 +0100

    updated this file

--- flexibleserver.tf
+++ flexibleserver.tf
@@ -1,14 +1,15 @@
 resource "azurerm_mysql_flexible_server" "this_rg_flexibleserver" {
-  name                   = var.flexible_server_name
+  name                   = "${local.owner}-${local.environment}-${var.flexible_server_name}"
   resource_group_name    = azurerm_resource_group.this_rg.name
   location               = azurerm_resource_group.this_rg.location
   administrator_login    = "mysqladminun"
   administrator_password = "H@Sh1CoR3!"
   sku_name               = "B_Standard_B1s"
+  zone = 1
 }
 
 resource "azurerm_mysql_flexible_database" "this_rg_flexibledb" {
-  name                = var.flexible_server_db
+  name                = "${local.owner}-${local.environment}-${var.flexible_server_db}"
   resource_group_name = azurerm_resource_group.this_rg.name
   server_name         = azurerm_mysql_flexible_server.this_rg_flexibleserver.name
   charset             = "utf8"
